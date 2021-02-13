89a23a157edc0c4d3648ffb06aadf72dcf05e904
Nayeem Hossain <nayeem9812@gmail.com>
2021-02-13 16:27:36 +0600

    homne page slider done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:travel_ui/constants/color_constant.dart';
+import 'package:travel_ui/constants/style_constant.dart';
+import 'package:travel_ui/models/slider_model.dart';
 import 'package:travel_ui/widgets/bottom_navigation.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,6 +12,17 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _currentSliderIndex = 0;
+
+  List<T> map<T>(List list, Function handler) {
+    List<T> result = [];
+
+    for (var i = 0; i < list.length; i++) {
+      result.add(handler(i, list[i]));
+    }
+    return result;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,8 +41,74 @@ class _HomeScreenState extends State<HomeScreen> {
           physics: ClampingScrollPhysics(),
           children: [
             Padding(
-              padding: EdgeInsets.only(left: 16, right: 16),
-              child: Text('Hi, user This promo is for you!',),
+              padding: EdgeInsets.only(left: 16, bottom: 16),
+              child: Text(
+                'Hi, user This promo is for you!',
+                style: titleStyle,
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.only(left: 16, right: 16),
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 195,
+                    child: Swiper(
+                      onIndexChanged: (index) {
+                        setState(() {
+                          _currentSliderIndex = index;
+                        });
+                      },
+                      autoplay: true,
+                      layout: SwiperLayout.DEFAULT,
+                      itemCount: sliders.length,
+                      itemBuilder: (BuildContext context, index) {
+                        return Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            image: DecorationImage(
+                              image: NetworkImage(sliders[index].image),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: map<Widget>(
+                          sliders,
+                          (index, image) {
+                            return Container(
+                              alignment: Alignment.centerLeft,
+                              height: 6,
+                              width: 6,
+                              margin: EdgeInsets.only(right: 8),
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: _currentSliderIndex == index
+                                      ? blueColor
+                                      : grayColor),
+                            );
+                          },
+                        ),
+                      ),
+                      Text('More..', style: discountStyle),
+                    ],
+                  )
+                ],
+              ),
             )
           ],
         ),
