5992eba8421d54256ad9ad7a10a5d99bd4bb7e66
Nayeem Hossain <nayeem9812@gmail.com>
2021-02-13 19:45:23 +0600

    destination model done

---
+++ lib/models/popular_destination_model.dart
@@ -0,0 +1,15 @@
+class PopularDestinationModel {
+  String name;
+  String country;
+  String image;
+
+  PopularDestinationModel(this.name, this.country, this.image);
+}
+
+List<PopularDestinationModel> destinations = destinationsData.map((item) => PopularDestinationModel(item['name'], item['country'], item['image'] )).toList();
+
+var destinationsData = [
+  {'name':'Bali', 'country':'Indonesia', 'image': 'https://udaipurmirror.com/wp-content/uploads/2020/06/flights-from1.png'},
+  {'name':'Paris', 'country':'France', 'image': 'https://i.pinimg.com/736x/ae/bc/91/aebc910ad397daa244792922b1917d25.jpg'},
+  {'name':'Istanbul', 'country':'Turkey', 'image': 'https://couponrim.com/pic/2018/12/trip-guru-xmas-holiday-discount-voucher-2019.jpg'},
+];
